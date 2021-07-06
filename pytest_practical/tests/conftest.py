655bd0260fca33aa66fc8d363a35151464d0980b
Mohit Agarwal <mohit.agarwal@nitorinfotech.com>
2021-07-06 12:26:23 +0530

    updating dependencies

--- pytest_practical/tests/conftest.py
+++ pytest_practical/tests/conftest.py
@@ -76,16 +76,16 @@ def env(request):
     return request.config.getoption("--env")
 
 
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-    cells.pop()
-
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.now(), class_='col-time'))
-    cells.pop()
+# def pytest_html_results_table_header(cells):
+#     cells.insert(2, html.th('Description'))
+#     cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+#     cells.pop()
+#
+#
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(1, html.td(datetime.now(), class_='col-time'))
+#     cells.pop()
 
 
 @pytest.hookimpl(hookwrapper=True)
